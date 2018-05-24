4eeddb175f444ec9445529e2bffa28dc915ca016
dfashbaugh <dylan.fashbaugh@gmail.com>
2018-05-23 22:40:38 -0400

    Initial commit

---
+++ HeepNintendoDemo/Assets/Heep/NonVolatileData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using UnityEngine;
+
+namespace Heep
+{
+	public static class NonVolatileData
+	{
+		public static void WriteMemoryToFile(List<Byte> memory)
+		{
+			// Create a file to write to.
+			File.WriteAllBytes ("DeviceMemory.bin", memory.ToArray ());
+		}
+
+		public static List<Byte> ReadMemoryFromFile()
+		{
+			try{
+				byte [] memArr = File.ReadAllBytes ("DeviceMemory.bin");
+				return new List<Byte> (memArr);
+			}
+			catch(FileNotFoundException) {
+				Debug.Log ("File not found. Creating new Device Memory File");
+				File.Create ("DeviceMemory.bin").Dispose();
+			}
+
+			return new List<Byte> ();
+		}
+	}
+}
+
---
+++ HeepNintendoDemo/Assets/HeepController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Heep;
+
+public class HeepController : MonoBehaviour {
+
+    HeepDevice myDevice;
+
+    public GameObject RightLight;
+    public GameObject LeftLight;
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log("Quitting!");
+        myDevice.CloseDevice();
+    }
+
+    void CreateDevice()
+    {
+        List<byte> ID = new List<byte>();
+        for(byte i = 0; i < 4; i++)
+        {
+            byte multiplier = 20;
+            ID.Add((byte)(i * multiplier));
+        }
+
+        DeviceID myID = new DeviceID(ID);
+        myDevice = new HeepDevice(myID);
+
+        myDevice.AddControl(Control.CreateControl(Control.CtrlInputOutput.input, Control.CtrlType.OnOff, "Left Light", false));
+        myDevice.AddControl(Control.CreateControl(Control.CtrlInputOutput.input, Control.CtrlType.OnOff, "Right Light", false));
+
+        myDevice.SetDeviceNameStartup("Room Game");
+        myDevice.StartListening();
+    }
+
+	// Use this for initialization
+	void Start () {
+        CreateDevice();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myDevice.GetControlValueByID(0) == 0)
+        {
+            LeftLight.GetComponent<Light>().enabled = false;
+        }
+        else
+        {
+            LeftLight.GetComponent<Light>().enabled = true;
+        }
+
+        if(myDevice.GetControlValueByID(1) == 0)
+        {
+            RightLight.GetComponent<Light>().enabled = false;
+        }
+        else
+        {
+            RightLight.GetComponent<Light>().enabled = true;
+        }
+	}
+}
