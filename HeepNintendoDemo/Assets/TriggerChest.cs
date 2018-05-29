83033d963331cd6264a0d0ee54c61dcd578a4aa5
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2018-05-29 15:28:09 -0400

    Added wait for seconds on the chest timer

--- HeepNintendoDemo/Assets/TriggerChest.cs
+++ HeepNintendoDemo/Assets/TriggerChest.cs
@@ -8,11 +8,20 @@ public class TriggerChest : MonoBehaviour {
     public GameObject OverlayImage;
     public GameObject FindingChestSound;
 
+	private IEnumerator GoHeepyTime()
+	{
+		OverlayImage.SetActive(true);
+		FindingChestSound.SetActive(true);
+
+		yield return new WaitForSeconds (5);
+
+		HeepControls.GetComponent<IslandHeep>().ChestTriggered();
+	}
+
     private void OnTriggerEnter(Collider other)
     {
-        HeepControls.GetComponent<IslandHeep>().ChestTriggered();
-        OverlayImage.SetActive(true);
-        FindingChestSound.SetActive(true);
-        Debug.Log("Hello Trigger");
+		Debug.Log("Hello Trigger");
+
+		StartCoroutine (GoHeepyTime ());
     }
 }
