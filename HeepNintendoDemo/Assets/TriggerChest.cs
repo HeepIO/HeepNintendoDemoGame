5761b6dc5369483bf606c50fcb4f618a086b86c0
dfashbaugh <dylan.fashbaugh@gmail.com>
2018-05-28 22:34:36 -0400

    Added fade out and zelda sound

--- HeepNintendoDemo/Assets/TriggerChest.cs
+++ HeepNintendoDemo/Assets/TriggerChest.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class TriggerChest : MonoBehaviour {
 
     public GameObject HeepControls;
+    public GameObject OverlayImage;
+    public GameObject FindingChestSound;
 
     private void OnTriggerEnter(Collider other)
     {
         HeepControls.GetComponent<IslandHeep>().ChestTriggered();
+        OverlayImage.SetActive(true);
+        FindingChestSound.SetActive(true);
         Debug.Log("Hello Trigger");
     }
 }
